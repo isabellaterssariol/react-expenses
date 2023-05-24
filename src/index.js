76af597196438633f0fb6b37fd3a852376a71e75
Isabella Terssariol <126811841+isabellaterssariol@users.noreply.github.com>
2023-05-24 16:33:14 -0300

    Iniciando projeto.
    Iniciando projeto de despesas com React.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-import App from './App';
+import App from './App'; // se for do arquivo .js podemos omitir esse .js
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root')); // cria uma constante que coloca a root (raíz), ou seja, o conteúdo que irá aparecer na página dentro da div 'root' que está no HTML //
+root.render(<App />); // diz o que deve ter dentro dessa div root, ou seja, qual conteúdo, nesse caso estamos importando do arquivo app.js
